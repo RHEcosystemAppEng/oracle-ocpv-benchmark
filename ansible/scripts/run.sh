436a639fdb8931df749f7339fe48a22f28c0c945
lrangine <19699092+lokeshrangineni@users.noreply.github.com>
2025-04-14 11:46:18 -0400

    Fixing the issues running the scripts on the target VM.

--- ansible/scripts/run.sh
+++ ansible/scripts/run.sh
@@ -13,6 +13,7 @@ source ./profile.sh
 export ORACLE_SYSTEM_PASSWORD=${ORACLE_SYSTEM_PASSWORD:-ChangePassw0rd}
 mkdir -p results/
 
-(time ../hammerdbcli auto benchmark_scripts/run.tcl | tee "benchmark_scripts/results/hammerdb_run_${BENCHNAME}.log")
+cd /opt/HammerDB/4.12
+(time ./hammerdbcli auto benchmark_scripts/run.tcl | tee "benchmark_scripts/results/hammerdb_run_${BENCHNAME}.log")
 
 grep -oP '[0-9]+(?= NOPM)' "./results/hammerdb_run_${BENCHNAME}.log" | tee -a "./results/hammerdb_nopm_${BENCHNAME}.log"
